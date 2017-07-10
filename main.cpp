6c6f5e11411a56ffae9bdff4de3d800c2b6d0492
Wes <willygoat95@gmail.com>
2017-07-10 07:11:53 -0600

    Rename variables

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <complex>
 #include <iostream>
-constexpr auto max_row = 22, max_column = 78, max_iteration = 20;
+constexpr auto rows = 22, columns = 78, iterations = 20;
 int main(){
-	for(auto row = 0; row < max_row; ++row){
-		for(auto column = 0; column < max_column; ++column){
+	for(auto row = 0; row < rows; ++row){
+		for(auto column = 0; column < columns; ++column){
 			std::complex<float> z, c = {
-				(float)column * 2 / max_column - 1.5f,
-				(float)row * 2 / max_row - 1				
+				(float)column * 2 / columns - 1.5f,
+				(float)row * 2 / rows - 1				
 			};
 			int iteration = 0;
-			while(abs(z) < 2 && ++iteration < max_iteration)
+			while(abs(z) < 2 && ++iteration < iterations)
 				z = pow(z, 2) + c;
-			std::cout << (iteration == max_iteration ? '#' : '.');
+			std::cout << (iteration == iterations ? '#' : '.');
 		}
 		std::cout << '\n';
 	}
